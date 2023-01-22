a72221d2c28550e9e8bdd48930a8075b28cf41ef
shubhams83 <shubhams@techis.io>
2023-01-22 08:29:20 +0530

    initial router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./containers/Home";
+import Cart from "./containers/Cart";
 
 const App = () => {
-  return <>Hello World</>;
+  return (
+    <>
+      <div className="app">
+        {/* Implement the Router Properties & Functionalities */}
+        <Router>
+          {/* Implement the Switching Routes, which means One Route View at a time */}
+          <Routes>
+            {/* Lay down the Route for each (Home) Container with exact props (Exact Path, Not a Part of URL) */}
+            <Route exact path="/cart" element={<Cart />} />
+            <Route exact path="/" element={<Home />} />
+            {/* Lay down the Route for each (Cart) Container with exact props (Exact Path, Not a Part of URL) */}
+          </Routes>
+        </Router>
+      </div>
+    </>
+  );
 };
 
 export default App;
